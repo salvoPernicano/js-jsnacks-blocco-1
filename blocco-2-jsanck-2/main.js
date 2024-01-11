1d8dab4ed944de16c4455cca0d4c71b14b65a6b8
salvoPernicano <salvo.pernicano@gmail.com>
2024-01-11 16:46:12 +0100

    correzione snack 3

--- blocco-2-jsanck-2/main.js
+++ blocco-2-jsanck-2/main.js
@@ -1,6 +1,6 @@
 const arrayNumeri = [2, 7, 1, 9, 4, 6];
 let riordine = arrayNumeri.filter(function(v,i){
-    return i % 2 == 0;
+    return i % 2 !== 0;
 });
 let somma = 0;
 for (let i=0; i < riordine.length; i++){

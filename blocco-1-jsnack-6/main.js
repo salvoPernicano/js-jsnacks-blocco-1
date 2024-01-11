c1cbd4b459a19b4b2e27bdcce9e17afb31c9693e
salvoPernicano <salvo.pernicano@gmail.com>
2024-01-11 16:14:17 +0100

    feat condizioni finali snack 6

--- blocco-1-jsnack-6/main.js
+++ blocco-1-jsnack-6/main.js
@@ -2,12 +2,15 @@ let userInput = prompt("give me a number");
 let userInputInt = parseInt(userInput);
 const arrayCubo = [];
 arrayCubo.push(userInputInt);
+let somma = userInputInt;
 
 
 for (let i = 1; i < userInputInt; i++){
     let CNumber = userInputInt - i;
     arrayCubo.push(CNumber);
+    somma += arrayCubo[i]
 }
+console.log(Math.pow(somma, 3));
 
-console.log(arrayCubo);
+
 

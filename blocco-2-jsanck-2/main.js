10174c90f1e234790e86d0bcf5074850e4e82a52
salvoPernicano <salvo.pernicano@gmail.com>
2024-01-11 17:24:36 +0100

    feat snack 2 blocco 2

---
+++ blocco-2-jsanck-2/main.js
@@ -0,0 +1,7 @@
+const  cognomi = ["Rossi", "Bianchi", "Verdi", "Neri", "Gialli"];
+const  nomi = ["Mario", "Luigi", "Giulia", "Anna", "Marco"];
+
+for (let i = 0; i < cognomi.length; i++){
+    console.log(cognomi[i] + " " + nomi[i])
+}
+

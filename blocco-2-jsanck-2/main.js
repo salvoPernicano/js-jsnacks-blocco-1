ea65dded26ebfee9bc2e8ae8f5a00f892dbf0e7d
salvoPernicano <salvo.pernicano@gmail.com>
2024-01-11 18:01:29 +0100

    fix interazione

--- blocco-2-jsanck-2/main.js
+++ blocco-2-jsanck-2/main.js
@@ -1,7 +1,9 @@
-const  cognomi = ["Rossi", "Bianchi", "Verdi", "Neri", "Gialli"];
-const  nomi = ["Mario", "Luigi", "Giulia", "Anna", "Marco"];
+const  cognomi =  ["Frodo", "Aragorn", "Gandalf", "Legolas", "Gimli", "Samwise", "Boromir", "Merry", "Pippin", "Faramir"];
+const  nomi = ["Baggins", "Strider", "Grey", "Greenleaf", "Gloin", "Gamgee", "Denethor", "Brandybuck", "Took", "Hethelion"];
+let numeroNomi = parseInt(prompt("quanti nomi vuoi?"))
 
-for (let i = 0; i < cognomi.length; i++){
-    console.log(cognomi[i] + " " + nomi[i])
+for (let i = 0; i < numeroNomi; i++){
+    let randomCognome = cognomi[Math.floor(Math.random()* cognomi.length)];
+    let randomNome = nomi[Math.floor(Math.random()*nomi.length)];
+    console.log(`${randomCognome} ${randomNome} `)
 }
-
